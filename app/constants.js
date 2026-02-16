7c05e8c60e64f94f2ec9466723df1357ae866121
Mark Steward Bayot <107757385+marksteward08@users.noreply.github.com>
2026-02-16 23:00:57 +0800

    fix: update SOCKET_URL to use secure ngrok link

--- app/constants.js
+++ app/constants.js
@@ -1,4 +1,5 @@
-const SOCKET_URL = "http://0.tcp.ap.ngrok.io:10530/";
+const SOCKET_URL =
+  "https://0b96-2400-d1e0-8000-3704-7975-dec3-6084-f0e1.ngrok-free.app";
 const DEFAULT_IMAGE_URL = "/default.jpg";
 
 export { SOCKET_URL, DEFAULT_IMAGE_URL };
