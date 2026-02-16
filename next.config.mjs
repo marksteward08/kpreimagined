23c40be12aed3f3382c27e713ae4e724efdb0b6a
Mark Steward Bayot <107757385+marksteward08@users.noreply.github.com>
2026-02-16 23:08:17 +0800

    fix: add allowedDevOrigins for local and ngrok development

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   /* config options here */
   reactCompiler: true,
   reactStrictMode: false,
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "https://0b96-2400-d1e0-8000-3704-7975-dec3-6084-f0e1.ngrok-free.app",
+  ],
 };
 
 export default nextConfig;
