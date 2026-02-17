14b15314120ac8f19340cdce597aad1467f6ec06
Mark Steward Bayot <107757385+marksteward08@users.noreply.github.com>
2026-02-18 05:24:40 +0800

    fix: changed SOCKET_URL to use ngrok link for development

--- app/constants.js
+++ app/constants.js
@@ -1,4 +1,5 @@
-const SOCKET_URL = "https://kprmbackend.vercel.app";
+const SOCKET_URL =
+  "https://ff25-2400-d1e0-8000-3704-5146-d8da-8ffe-799.ngrok-free.app/";
 const DEFAULT_IMAGE_URL = "/default.jpg";
 const DEBUG_MODE = false;
 
